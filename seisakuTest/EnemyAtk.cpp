b8233df16520e7cb9f9796702a923df50fba0aae
kuroneko96 <kaityougakusyuuyou@gmail.com>
2019-11-15 11:54:44 +0900

    MainPCへ送ります

--- seisakuTest/EnemyAtk.cpp
+++ seisakuTest/EnemyAtk.cpp
@@ -5,12 +5,18 @@
 #define PI 3.14
 
 int zako;
+int goei;
 int player;
+int speed = 2;
 int Image[50];
 double zakoX;
 double zakoY;
+double goeiX;
+double goeiY;
 double zakomoveX;
 double zakomoveY;
+double goeimoveX;
+double goeimoveY;
 double playerX;
 double playerY;
 
@@ -21,29 +27,76 @@ void Update() {
 	double tmpplayerY;
 	static double tmpzakoX = zakomoveX;
 	static double tmpzakoY = zakomoveY;
-	static int r = 0;
-	static int r2;
-	static int flg = 1;
-	static int flg2 = 1;
+	static double r = 0;
+	static double r2;
+	static double r3;
+	static double r4 = 360;
+	static int flg = 0;
+	static int flg2 = 0;
+	static int flg3 = 0;
+	static int flg4 = 1;
 	tmpplayerX = playerX;
 	tmpplayerY = playerY;
 	rad = r * PI / 180;
-	r2 = atan2(zakoY - playerY, zakoX - playerX);
+	rad02 = r4 * PI / 180;
+	r2 = atan2(tmpplayerY - zakoY, (tmpplayerX + 30) - zakoX);
+	r3 = atan2(tmpzakoY - zakoY, tmpzakoX - zakoX);
 
-	if (flg2 == 1) {
-		zakoX -= 2 * cos(r2);
-		zakoY -= 2 * sin(r2);
+	//“G‚Ì‚Ù‚¤‚Ö‚Ì•ûŒü“]Š·
+	if (flg4 == 1) {
+		zakoX = tmpzakoX + 30 * cos(rad02);
+		zakoY = tmpzakoY + 30 * sin(rad02);
+		if (r4 < 180) {
+			flg4 = 0;
+			flg2 = 1;
+		}
+		r4 -= 2;
 	}
-	if (flg == 0) {
+
+	//“G‚Ì‚Ù‚¤‚Ö‚Ìs“®
+	if (flg2 == 1) {
+		if (zakoY > playerY) {
+			flg2 = 0;
+			flg = 1;
+		}
+		else {
+			zakoX += speed * cos(r2);
+			zakoY += speed * sin(r2);
+		}
+	}
+	//“G‚Ì‹ß‚­‚Å‰ñ“]
+	if (flg == 1) {
 		zakoX = tmpplayerX + 30 * cos(rad);
 		zakoY = tmpplayerY + 30 * sin(rad);
-		r++;
+		if (r >= 150) {
+			flg = 0;
+			flg3 = 1;
+		}
+		else {
+			r += 2;
+		}
+	}
+	//‘à—ñ‚Ö–ß‚é
+	if (flg3 == 1) {
+		if (zakoX < tmpzakoX && zakoY < tmpzakoY) {
+			flg3 = 0;
+		}
+		else {
+			zakoX += speed * cos(r3);
+			zakoY += speed * sin(r3);
+		}
 	}
 }
 
+void Draw_Goei() {
+	goeiX += speed;
+	goeiY += speed;
+}
+
 void Draw() {
-	DrawRotaGraph(zakoX, zakoY, 2.0f, 0, zako, TRUE);
-	DrawRotaGraph(playerX, playerY, 2.0f, 0, player, TRUE);
+	DrawRotaGraph((int)playerX, (int)playerY, 2.0f, 0, player, TRUE);
+	DrawRotaGraph((int)zakoX, (int)zakoY, 2.0f, 0, zako, TRUE);
+	DrawRotaGraph((int)goeiX, (int)goeiY, 2.0f, 0, goei, TRUE);
 }
 
 void Game() {
@@ -52,22 +105,28 @@ void Game() {
 
 	LoadDivGraph("‰æ‘œ/Galaga_OBJ_enemy.png", 50, 5, 10, 17, 18, Image);
 	zako = Image[0];
+	goei = Image[5];
 	player = Image[10];
 	zakoX = 100;
 	zakoY = 200;
+	goeiX = 150;
+	goeiY = 150;
 	zakomoveX = zakoX;
 	zakomoveY = zakoY;
+	goeimoveX = goeiX;
+	goeimoveY = goeiY;
 	playerX = 400;
 	playerY = 750;
 
 
 	while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0) {
 		Draw();
-		Update();
+		//Update();
+		Draw_Goei();
 	}
 }
 
-int WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	ChangeWindowMode(TRUE), DxLib_Init(), SetDrawScreen(DX_SCREEN_BACK); //ƒEƒBƒ“ƒhƒEƒ‚[ƒh•ÏX‚Æ‰Šú‰»‚Æ— ‰æ–ÊÝ’è
 
 	SetGraphMode(1100, 800, 32);
