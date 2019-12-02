2dedb4a232d3d9e3232e294d362e6b0ee74afe49
bontenmarukamisama <bontenmarukamisama@gmail.com>
2019-12-02 17:00:37 +0900

    スコア修正

--- Project1 - 神/Score.cpp
+++ Project1 - 神/Score.cpp
@@ -1,15 +1,10 @@
 #include "DxLib.h"
 #include "Score.h"
 
-typedef struct hiscore
-{
-	int score = 0;                          //スコア
-	int h_score = 0;                       //ハイスコア
-	char String[256];                     //保存変数
 
-}HISCORE;
-
-HISCORE high_score;
+int score = 0;                          //スコア
+int h_score = 0;                       //ハイスコア
+char String[256];                     //保存変数
 
 int scorex = 950, scorey = 200;            //スコア計算の座標
 int high_scorex = 950, high_scorey = 90;  //ハイスコア計算座標
@@ -21,7 +16,7 @@ FILE* fp;
 
 //更新
 void Score_Update() {
-	high_score.score = 0;
+	score = 0;
 }
 
 //描画
@@ -31,10 +26,10 @@ void Score_Save() {
 	FileHandle = FileRead_open("h_score.dat");
 
 	// 一行読む
-	FileRead_gets(high_score.String, 256, FileHandle);
+	FileRead_gets(String, 256, FileHandle);
 
 	// 画面に描画
-	DrawString(high_scorex, high_scorey, high_score.String, White);
+	DrawString(high_scorex, high_scorey, String, White);
 
 	// ファイルを閉じる
 	FileRead_close(FileHandle);
@@ -46,7 +41,7 @@ void Score_Draw() {
 
 	//ハイスコアの記録
 	fp = fopen("h_score.dat", "wb");
-	fprintf(fp, "%d", high_score.h_score);
+	fprintf(fp, "%d", h_score);
 
 	fclose(fp);
 
@@ -60,22 +55,22 @@ void Score_Draw() {
 	DrawFormatString(900, 160, Red, "1UP");
 
 	//スコア数字の描画
-	DrawFormatString(scorex, scorey, White, "%d", high_score.score);
+	DrawFormatString(scorex, scorey, White, "%d", score);
 
 	// ハイスコアの数字描画char型
-	DrawFormatString(high_scorex, high_scorey, White, "%s", high_score.String);
+	DrawFormatString(high_scorex, high_scorey, White, "%s", String);
 
 	//char型からint型への変換
-	high_score.h_score = atoi(high_score.String);
+	h_score = atoi(String);
 
 	//ハイスコア更新、スコア連動
-	if (high_score.h_score < high_score.score) {
+	if (h_score < score) {
 		SetDrawScreen(DX_SCREEN_BACK);  //塗りつぶし                        
 		DrawBox(high_scorex, high_scorey, 1000, 150, Black, TRUE);
 
 		//塗りつぶし後の描画
-		high_score.h_score = high_score.score;
-		DrawFormatString(high_scorex, high_scorey, White, "%d", high_score.h_score);
+		h_score = score;
+		DrawFormatString(high_scorex, high_scorey, White, "%d", h_score);
 	}
 
 }
@@ -86,13 +81,13 @@ void Score_Title() {
 	FileHandle = FileRead_open("h_score.dat");
 
 	// 一行読む
-	FileRead_gets(high_score.String, 256, FileHandle);
+	FileRead_gets(String, 256, FileHandle);
 
 	// 画面に描画
-	DrawString(140, 90, high_score.String, White);
+	DrawString(140, 90, String, White);
 
 	// 画面に描画
-	DrawString(390, 90, high_score.String, White);
+	DrawString(390, 90, String, White);
 
 	// ファイルを閉じる
 	FileRead_close(FileHandle);
