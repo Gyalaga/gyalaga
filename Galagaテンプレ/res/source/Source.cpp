6c83a44a38c134a04e9396f03fb8411adf38f5fe
Flamberg3983 <masaki1217jp@gmail.com>
2019-12-20 09:45:56 +0900

    GalagaテンプレがGalagaのテンプレ

---
+++ Galagaテンプレ/res/source/Source.cpp
@@ -0,0 +1,12 @@
+#include "DxLib.h"
+#include <Windows.h>
+
+ int WINAPI WinMain(HINSTANCE hInstance,
+					HINSTANCE hPrevInstance,
+					LPSTR lpCmdLine,
+					int nCmdShow) 
+ {
+
+	MessageBox(NULL, TEXT("ÇÅÇÅÇÅÇÅÇÅÇÅÇÅ"), TEXT("aaaaaa"),MB_OK);
+	return 0;
+ }
